05395816dd750b2da3a528b8cea0dd5a55c287df
AlexTraveylan <monsieurdemares@gmail.com>
2025-03-10 22:32:49 +0100

    feature: db config + changelog

--- fastauth/db/database.py
+++ fastauth/db/database.py
@@ -1,29 +1,35 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlmodel import SQLModel
 
-from fastauth.config.settings import settings
-
-engine = create_async_engine(
-    settings.DATABASE_URL,
-    echo=settings.DEBUG,
-    future=True,
-)
-
-async_session = async_sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
+from fastauth.common.settings import settings
 
 
-async def init_db():
+def get_async_engine() -> AsyncEngine:
+    """Get the async engine for the database."""
+    return create_async_engine(
+        settings.DATABASE_URL,
+        echo=settings.DEBUG,
+        future=True,
+    )
+
+
+async def init_db() -> None:
     """Initialize the database with the defined models."""
-    async with engine.begin() as conn:
-        # SQLModel.metadata.drop_all(conn)  # Uncomment to reset the database
-        await conn.run_sync(SQLModel.metadata.create_all)
+    async_engine = get_async_engine()
+
+    async with async_engine.begin() as async_conn:
+        await async_conn.run_sync(SQLModel.metadata.create_all)
 
 
-async def get_session():
-    """Create a database session for FastAPI dependencies."""
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    """Yield an async session."""
+    async_session = async_sessionmaker(
+        bind=get_async_engine(),
+        autoflush=False,
+        expire_on_commit=False,
+    )
     async with async_session() as session:
         yield session
